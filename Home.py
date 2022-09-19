0c10cda9ade0e93adfa984ff2d38143d1bc9fa38
William Mattingly <62964060+wjbmattingly@users.noreply.github.com>
2022-09-19 19:10:32 +0200

    init

---
+++ Home.py
@@ -0,0 +1,81 @@
+import streamlit as st
+import pandas as pd
+from txtai.embeddings import Embeddings
+import gdown
+import os
+
+
+def download_files():
+    url_full = "https://drive.google.com/file/d/1ySyspDK7g1DQIVbegpDOpwv9FH73smiB/view?usp=sharing"
+
+    if os.path.exists("./models/oral_testimonies/embeddings"):
+        pass
+    else:
+        output = "./models/oral_testimonies/embeddings"
+        gdown.download(url_full, output, quiet=False, fuzzy=True)
+        print("Download Complete")
+
+# download_files()
+
+
+@st.cache(allow_output_mutation=True)
+def load_txtai():
+    embeddings = Embeddings({"path": "sentence-transformers/all-MiniLM-L6-v2", "content": True})
+    embeddings.load("models/annual_reports")
+    return embeddings
+
+st.title("Smithsonian Annual Reports")
+st.sidebar.image("images/logo.png")
+with open ("markdown/description.md", "r") as f:
+    description = f.read()
+st.sidebar.markdown(description, unsafe_allow_html=True)
+query = st.sidebar.text_input("Query")
+num_results = st.sidebar.number_input("Number of Results", 1, 2000, 20)
+
+embeddings = load_txtai()
+
+
+def create_html(result):
+    output = f""
+    spans = []
+    for token, score in result["tokens"]:
+        color = None
+        if score >= 0.1:
+            color = "#fdd835"
+        elif score >= 0.075:
+            color = "#ffeb3b"
+        elif score >= 0.05:
+            color = "#ffee58"
+        elif score >= 0.025:
+            color = "#fff59d"
+
+        spans.append((token, score, color))
+
+    if result["score"] >= 0.05 and not [color for _, _, color in spans if color]:
+        mscore = max([score for _, score, _ in spans])
+        spans = [(token, score, "#fff59d" if score == mscore else color) for token, score, color in spans]
+
+    for token, _, color in spans:
+        if color:
+            output += f"<span style='background-color: {color}'>{token}</span> "
+        else:
+            output += f"{token} "
+    return output
+
+
+if st.sidebar.button("Search"):
+    res = embeddings.explain(query, limit = num_results)
+
+    html_txt = [create_html(r) for r in res]
+    indices = [index['id'] for index in res]
+
+    scores = [index['score'] for index in res]
+    texts = [index['text'] for index in res]
+    data = {
+            "index": indices,
+            "similarity": scores,
+            "text": html_txt
+    }
+    df = pd.DataFrame(data)
+
+    st.markdown(df.to_markdown(), unsafe_allow_html=True)
---
+++ markdown/description.md
@@ -0,0 +1,5 @@
+Developed by [the Smithsonian's Data Science Lab](https://datascience.si.edu/) using [Streamlit](https://www.streamlit.io) and [txtAI](https://github.com/neuml/txtai).
+
+A small subset of the Annual Reports are currently used for this beta version of the application. Due to the varied OCR quality of the annual reports, not all text data is available for each report. Only sentences larger than 7 words were indexed.
+
+Version 0.0.2
